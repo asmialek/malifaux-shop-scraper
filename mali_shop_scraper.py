793261b27467afee16aee6f26eac4858f5e4a5c9
Adam Śmiałek <smialekadam@gmail.com>
2022-09-06 17:15:25 +0200

    Update

--- mali_shop_scraper.py
+++ mali_shop_scraper.py
@@ -9,6 +9,7 @@ import tabulate
 import requests as r
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def check_retailer(url, i):
@@ -32,6 +33,7 @@ def find_items(url, range_max, product_tag, name_tag, availability_tag, availabi
         if products == last_page_contents or products == None: break
         for product in products:
             name = product.find(class_=name_tag).text
+            print(name)
             try:
                 availability = availability_string in product.find(class_=availability_tag).text
             except AttributeError:
@@ -56,15 +58,36 @@ def find_items_szopgracz(url, product_tag, name_tag, availability_tag, availabil
 
 # %%
 def find_items_allegro():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://allegro.pl/kategoria/bitewne-inne-systemy-6059?string=malifaux&p=1")
-    
+    driver = webdriver.Chrome(ChromeDriverManager().install())  
     html = driver.page_source
     soup = bs(html, "html.parser")
     product_list = []
     for i in range(1,30):
         new_products = 0
         driver.get(f"https://allegro.pl/kategoria/bitewne-inne-systemy-6059?string=malifaux&p={i}")
+        html = driver.page_source
+        soup = bs(html, "html.parser")
+        products = soup.find_all(class_='mgn2_14 m9qz_yp meqh_en mpof_z0 mqu1_16 m6ax_n4 mp4t_0 m3h2_0 mryx_0 munh_0 mj7a_4')
+        for product in products:
+            name = product.text
+            if name not in product_list:
+                # print(name)
+                product_list.append(name)
+                new_products += 1
+        print('Page:', i, end='\r')
+        if new_products == 0: break
+    
+    return product_list
+
+# %%
+def used_items_allegro():
+    driver = webdriver.Chrome(ChromeDriverManager().install())  
+    html = driver.page_source
+    soup = bs(html, "html.parser")
+    product_list = []
+    for i in range(1,30):
+        new_products = 0
+        driver.get(f"https://allegro.pl/listing?string=malifaux&stan=używane")
         html = driver.page_source
         soup = bs(html, "html.parser")
         products = soup.find_all(class_='mgn2_14 m9qz_yp meqh_en mpof_z0 mqu1_16 m6ax_n4 mp4t_0 m3h2_0 mryx_0 munh_0 mj7a_4')
@@ -81,7 +104,56 @@ def find_items_allegro():
     return product_list
 
 # %%
-def create_shop_dict():
+def find_items_levi():
+    driver = webdriver.Chrome(ChromeDriverManager().install())  
+    last_page_contents = None
+    available_products_list = set()
+    
+    html = driver.page_source
+    soup = bs(html, "html.parser")
+    for i in range(1,30):
+        driver.get(f"https://levisuel.com/fr/4-malifaux?page={i}")
+        html = driver.page_source
+        soup = bs(html, "html.parser")
+        products = soup.find_all(class_='product-miniature js-product-miniature')
+        if products == last_page_contents or products == None: break
+        for product in products:
+            name = product.find(class_='h3 product-title').text
+            try:
+                availability = 'Ajouter' in product.find(class_='add-to-cart').text
+            except AttributeError:
+                continue
+            if availability:
+                available_products_list.add(name.strip())
+        print('Page:', i, end='\r')
+    
+        last_page_contents = products
+    return available_products_list
+
+# %%
+def find_items_fire():
+    driver = webdriver.Chrome(ChromeDriverManager().install())  
+    available_products_list = set()
+    
+    html = driver.page_source
+    soup = bs(html, "html.parser")
+    driver.get(f"https://www.firestormgames.co.uk/wargames-miniatures/malifaux")
+    html = driver.page_source
+    soup = bs(html, "html.parser")
+    products = soup.find_all(class_='item-inner')
+    for product in products:
+        name = product.find(class_='title match-height').text
+        try:
+            availability = 'Now' in product.find(class_='banner alert alert-info').text
+        except AttributeError:
+            continue
+        if availability:
+            available_products_list.add(name.strip())
+    
+    return available_products_list
+
+# %%
+def create_shop_dict(allegro=True, abroad=True):
     shops_dict = {}
 
     print('\ngraal')
@@ -93,20 +165,34 @@ def create_shop_dict():
     shops_dict['bolter'] = bolter
 
     print('\nstrefa')
-    strefamtg = find_items('https://strefamtg.pl/2596-malifaux-3ed?page=', 30, 'product-miniature__container', 'product-miniature__title', 'product-quantities', 'Dostępne')
+    strefamtg = find_items('https://strefamtg.pl/2596-malifaux-3ed?page=', 30, 'product-miniature__container', 'product-miniature__title', 'product-quantities', 'dostępna')
     shops_dict['strefa'] = strefamtg
 
     print('\nszop')
     szopgracz = find_items_szopgracz('https://shopgracz.pl/298-malifaux', 'item-product', 'product_name', 's_soon', 'niedostępny')
     shops_dict['szop'] = szopgracz
 
-    # print('\ngnom')
-    # gnom = find_items('https://gnom-sklep.pl/447-malifaux-3rd-edition?page=', 30, 'product-miniature', 'h3', 'hiaddtocart', 'Dostępny')
-    # shops_dict['gnom'] = gnom
+    print('\ngnom')
+    gnom = find_items('https://gnom-sklep.pl/pl/menu/malifaux-261.html?filter_price=&filter_availability=y?counter=', 10, 'product', 'product__name', 'product__name', '')
+    shops_dict['gnom'] = gnom
 
-    # print('\nallegro')
-    # allegro = find_items_allegro()
-    # shops_dict['allegro'] = allegro
+    if allegro:
+        print('\nallegro')
+        allegro = find_items_allegro()
+        shops_dict['allegro'] = allegro
+
+    if abroad:
+        print('\nlevisuel')
+        levi = find_items_levi()
+        shops_dict['levi'] = levi
+
+        print('\nminiatur')
+        miniatur = find_items('https://www.miniaturicum.de/3rd-Edition?availability=1&seite=', 30, 'productbox-inner', 'productbox-title', 'price-note', '')
+        shops_dict['miniatur'] = miniatur
+
+        print('\nfirestorm')
+        firestorm = find_items_fire()
+        shops_dict['firestorm'] = firestorm
     
     return shops_dict
 
@@ -126,55 +212,65 @@ def generate_table(item_list, shops_dict):
                 else:
                     table_row.append('-')
         table_rows.append([item.title(), *table_row])
-    return(tabulate.tabulate(table_rows, headers=['box', *shops_dict.keys()], tablefmt="pretty"))
+    print(tabulate.tabulate(table_rows, headers=['box', *shops_dict.keys()], tablefmt="pretty"))
 
 # %%
 def main(item_list):
     shops_dict = create_shop_dict()
-    return generate_table(item_list, shops_dict)
+    generate_table(item_list, shops_dict)
 
 # %% [markdown]
 # ## Dictionary generation
+
+# %%
+shops_dict = create_shop_dict()
+
+# %%
+shops_dict
 
 # %% [markdown]
 # ## Table generation
 
 # %%
 item_list = ['Tara Core Box',
+             'Forward and Back',
              'Servants of the Void',
              'Beyond Time',
              '---------------------',
              'KAERIS CORE BOX',
-             'BURNING BRIDGES', 
+             'BURNING BRIDGES',
+             'Mechanical Rider',
+             'Center Stage',
+             'CRACK IN THE FOUNDATION',
+             'EMBRACE THE EMBER',
              '---------------------',
              'LADY JUSTICE CORE BOX',
              'WAKE THE DEAD',
              '---------------------',
-             'SONNIA CORE BOX',
-             'WITCHING HOUR',
-             '---------------------',
-             'SANDEEP CORE BOX',
-             'PEER REVIEW',
-             '---------------------',
              'VON SCHILL CORE BOX',
              'BETWEEN THE LEY LINES',
+             'CUTTING-EDGE TECHNOLOGY',
              '---------------------',
-             'Misaki Core Box',
-             'CRIME SYNDICATE',
-             'Silent Strike',
-             'Dawn Serpent',
-             'Dawn Sepent',
-             'Kharmic Debt',
-             'Karmic Debt',
+             'LEVETICUS CORE BOX',
+             'NECROPUNKS',
+             'DESOLATION',
+             'ASHES AND DUST',
+             'MARLENA WEBSTER',
              '---------------------',
-             'ASAMI CORE BOX',
-             'ANCIENT EVIL',
-             'Immortal Tricksters',
-             'Akaname',
+             'HOFFMAN CORE BOX',
+             'SUPPORT STAFF',
+             'KEEPING THE PEACE',
              '---------------------',
-             'JEDZA CORE BOX',
-             'HERE LIES...',
-             'HERE LIES',
+             'Intrepid Fate',
+             '---------------------',
+             'Ravencroft Core Box',
+             'Invisible Ink',
+             'Living Soulstones',
+             '---------------------',
+             'Lord Cooper Core Box',
+
              ]
+
+generate_table(item_list, shops_dict)
 
 
